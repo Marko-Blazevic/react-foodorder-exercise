c4db4674dc3017e1e859179fabb9d63a51777982
Marko Blazevic <www.markoblazevic@gmail.com>
2022-03-24 10:10:33 -0300

    Added Header Cart Button animation when adding meals

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const CartButton = (props) => {
+  const [buttonIsHighlited, setButtonIsHighlited] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlited ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlited(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlited(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.showCart}>
+    <button className={btnClasses} onClick={props.showCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
